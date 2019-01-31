550bb160da301d8813cd9b02da058916373ddf2e
Hoshikawa <luckyhiroaki@icloud.com>
2019-01-31 17:13:24 +0900

    update

---
+++ Assets/Scripts/NomalBallScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NomalBallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        bool isShift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        if (other.gameObject.name == "key1" && Input.GetKeyDown(KeyCode.D) && isShift == false)
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.name == "Key2" && Input.GetKeyDown(KeyCode.F) && isShift == false)
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.name == "Key3" && Input.GetKeyDown(KeyCode.K) && isShift == false)
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.name == "Key4" && Input.GetKeyDown(KeyCode.L) && isShift == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

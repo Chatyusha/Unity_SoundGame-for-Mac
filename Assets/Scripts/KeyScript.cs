20de4a4083bb7d2ce318322ea248bebf1201e465
Hoshikawa <luckyhiroaki@icloud.com>
2019-01-31 20:02:31 +0900

    update

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+    public GameObject key1;
+    public GameObject key2;
+    public GameObject key3;
+    public GameObject key4;
+
+    ParticleSystem ps1;
+    ParticleSystem ps2;
+    ParticleSystem ps3;
+    ParticleSystem ps4;
+	// Use this for initialization
+	void Start () {
+        ps1 = key1.GetComponent<ParticleSystem>();
+        ps2 = key2.GetComponent<ParticleSystem>();
+        ps3 = key3.GetComponent<ParticleSystem>();
+        ps4 = key4.GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.D)){
+            ps1.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ps2.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            ps3.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ps4.Play();
+        }
+
+    }
+}
---
+++ Assets/Scripts/SPBallScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPBallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        bool shiftkey = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        if(other.name=="Key1"&&Input.GetKeyDown(KeyCode.D)&&shiftkey==true){
+            Destroy(gameObject);
+        }else if (other.name == "Key2" && Input.GetKeyDown(KeyCode.F) && shiftkey == true)
+        {
+            Destroy(gameObject);
+        }else if (other.name == "Key3" && Input.GetKeyDown(KeyCode.J) && shiftkey == true)
+        {
+            Destroy(gameObject);
+        }else if (other.name == "Key4" && Input.GetKeyDown(KeyCode.K) && shiftkey == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

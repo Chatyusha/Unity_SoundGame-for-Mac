57bddf109c58bec5f14e9d138be148e59ec44482
Hoshikawa <luckyhiroaki@icloud.com>
2019-02-01 15:39:53 +0900

    update 2109/02/01/ 15:39

--- Assets/Scripts/PlayerMoveScript.cs
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -2,16 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoveScript : MonoBehaviour {
+public class PlayerMoveScript : MonoBehaviour
+{
+    [SerializeField, Range(0, 60*3+42)]
+    float time = 1;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Vector3 v = transform.position;
+    [SerializeField]
+    Vector3 endPosition;
 
-	}
+    //[SerializeField]
+    //AnimationCurve curve;
+
+    private float startTime;
+    private Vector3 startPosition;
+
+    // Use this for initialization
+    void Start(){
+
+    }
+    void OnEnable()
+    {
+        if (time <= 0)
+        {
+            transform.position = endPosition;
+            enabled = false;
+            return;
+        }
+
+        startTime = Time.timeSinceLevelLoad;
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var diff = Time.timeSinceLevelLoad - startTime;
+        if (diff > time)
+        {
+            transform.position = endPosition;
+            enabled = false;
+        }
+
+        var rate = diff / time;
+        //var pos = curve.Evaluate(rate);
+
+        transform.position = Vector3.Lerp(startPosition, endPosition, rate);
+        //transform.position = Vector3.Lerp (startPosition, endPosition, pos);
+    }
 }

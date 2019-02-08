da2c43a41dac7fb2313b645d636f080d43ef7041
Hoshikawa <luckyhiroaki@icloud.com>
2019-02-08 17:38:43 +0900

    update

--- Assets/Scripts/NomalBallScript.cs
+++ Assets/Scripts/NomalBallScript.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NomalBallScript : MonoBehaviour {
-
+    public GameObject canvas;
+    UIScript uiscript;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,25 +12,29 @@ public class NomalBallScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        uiscript = canvas.GetComponent<UIScript>();
 	}
     private void OnTriggerStay(Collider other)
     {
        
         if (other.gameObject.name == "Key1"&&Input.GetKeyDown(KeyCode.D))
         {
+            uiscript.countN();
             Destroy(gameObject);
         }
         else if (other.gameObject.name == "Key2" && Input.GetKeyDown(KeyCode.F))
         {
+            uiscript.countN();
             Destroy(gameObject);
         }
         else if (other.gameObject.name == "Key3" && Input.GetKeyDown(KeyCode.J))
         {
+            uiscript.countN();
             Destroy(gameObject);
         }
         else if (other.gameObject.name == "Key4" && Input.GetKeyDown(KeyCode.K))
         {
+            uiscript.countN();
             Destroy(gameObject);
         }
     }
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+    public Text scoreT;
+    int score;
+	// Use this for initialization
+	void Start () {
+        scoreT.text = "SCORE : 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void countN(){
+        score += 100;
+        scoreT.text = "SCORE : " + score;
+    }
+}

4aa883261c50a95fffa1b819c60658cf12b02872
Hoshikawa <luckyhiroaki@icloud.com>
2019-02-09 19:50:10 +0900

    updateupdate

---
+++ Assets/Scripts/PlusPoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlusPoint : MonoBehaviour {
+    public GameObject canvas;
+    UIScript ui;
+	// Use this for initialization
+	void Start () {
+        ui = canvas.GetComponent<UIScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
